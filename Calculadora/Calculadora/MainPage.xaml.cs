e1280c1f8cb7bb5eeb83d67328e1361de3f0f640
Jean Pandolfi <jean-pandolfi@hotmail.com>
2021-03-09 03:22:51 -0300

    first commit

---
+++ Calculadora/Calculadora/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void dizerNomeCompleto(object sender, EventArgs e)
+        {
+            var text1 = nome.Text;
+            var text2 = sobrenome.Text;
+
+            DisplayAlert("Nome Completo!", text1 + " " + text2, "OK");
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# sample_app_mobile_xamarin
